4d02b551eb2fd7f666d40d3bc07aa3e126408caf
shivanshlamba <shivansh.lamba@gmail.com>
2025-11-22 14:15:45 +0530

    Update functions.js

--- file/functions.js
+++ file/functions.js
@@ -1,11 +1,9 @@
-
-
 // variables
 var $win = $(window);
 var clientWidth = $win.width();
 var clientHeight = $win.height();
 
-$(window).resize(function() {
+$(window).resize(function () {
     var newWidth = $win.width();
     var newHeight = $win.height();
     if (newWidth != clientWidth && newHeight != clientHeight) {
@@ -13,50 +11,57 @@ $(window).resize(function() {
     }
 });
 
-(function($) {
-	$.fn.typewriter = function() {
-		this.each(function() {
-			var $ele = $(this), str = $ele.html(), progress = 0;
-			$ele.html('');
-			var timer = setInterval(function() {
-				var current = str.substr(progress, 1);
-				if (current == '<') {
-					progress = str.indexOf('>', progress) + 1;
-				} else {
-					progress++;
-				}
-				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
-				if (progress >= str.length) {
-					clearInterval(timer);
-				}
-			}, 75);
-		});
-		return this;
-	};
+// TYPEWRITER EFFECT (working)
+(function ($) {
+    $.fn.typewriter = function () {
+        this.each(function () {
+            var $ele = $(this),
+                str = $ele.html(),
+                progress = 0;
+            $ele.html('');
+            var timer = setInterval(function () {
+                var current = str.substr(progress, 1);
+                if (current == '<') {
+                    progress = str.indexOf('>', progress) + 1;
+                } else {
+                    progress++;
+                }
+                $ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+                if (progress >= str.length) {
+                    clearInterval(timer);
+                }
+            }, 75);
+        });
+        return this;
+    };
 })(jQuery);
 
-function timeElapse(date){
-	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
-	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	seconds = seconds % 60;
-	if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
-	var result = "Days <span class=\"digit\">" + days + "</span> Hours <span class=\"digit\">" + hours + "</span> Minutes <span class=\"digit\">" + minutes; 
-	$("#clock").html(result);
+// ------------------------------------------------------
+// CLOCK FIX + WORKING UPDATE + MATCHES YOUR HTML
+// ------------------------------------------------------
 
-	var text = "THE WORLD JUST GOT LUCKIER SINCE ";
-	$("#message-box").html(text);
+function updateClock() {
+    // DATE YOU WANT TO START COUNTING FROM
+    // Example: 9497 days ago = around 1999. Use your date here:
+    var startDate = new Date("1999-11-23T00:00:00");
 
+    var now = new Date();
+    var seconds = Math.floor((now - startDate) / 1000);
+
+    var days = Math.floor(seconds / (3600 * 24));
+    seconds %= 3600 * 24;
+
+    var hours = Math.floor(seconds / 3600);
+    seconds %= 3600;
+
+    var minutes = Math.floor(seconds / 60);
+    seconds %= 60;
+
+    // Format the clock EXACTLY as your index.html expects
+    var text = days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds";
+
+    $("#clock").html(text);
 }
+
+// Update clock every second
+setInterval(updateClock, 1000);
